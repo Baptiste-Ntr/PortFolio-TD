bc024a828aa3d3d7d9ea99e9d9a4db82366ec23e
Baptiste-Ntr <98717656+Baptiste-Ntr@users.noreply.github.com>
2022-11-05 13:42:06 +0100

    90%

--- Multi Page/JS/redirection.js
+++ Multi Page/JS/redirection.js
@@ -4,4 +4,19 @@ var nav_contact = document.getElementById('nav_contact');
 
 nav_projet.addEventListener("click", () => {
     window.location.href = "HTML/projets.html";
-})
+});
+
+nav_cv.addEventListener("click", () => {
+    window.location.href = "HTML/cv.html";
+});
+
+nav_contact.addEventListener("click", () => {
+    window.location.href = "HTML/contact.html";
+});
+
+var main = document.getElementById('bn');
+
+main.addEventListener("click", () => {
+    window.location.href = "../index.html";
+    console.log(window.location.href = "../index.html");
+});
